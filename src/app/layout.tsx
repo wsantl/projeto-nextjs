eb362126b5d2c52dd7741858c80217e10486d39a
wsantl <wsantoslba@hotmail.com>
2025-02-05 09:48:02 -0300

    add header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,8 @@ export default function RootLayout({
       <body
         className={`antialiased`}>
 
+        <Header />
+
         {children}
 
       </body>
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,22 @@
 export function Header() {
     return (
         <header>
-            <h1>HEADER DO PROJETO</h1>
+            <div className="flex px-2 py-4 bg-zinc-900 text-white">
+                <div className="flex items-center justify-between w-full mx-auto max-w-7x1">
+                    NextJS
+                </div>
+
+                <nav>
+                    <ul className="flex items-center justify-center gap-2">
+                        <li>
+                            Home
+                        </li>
+                        <li>
+                            Posts
+                        </li>
+                    </ul>
+                </nav>
+            </div>
         </header>
     )
 }
